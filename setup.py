9bb40c6756e7ee6641565bc680558ecb1a4b1178
Trey Hunner <treyhunner@gmail.com>
2012-03-16 08:18:46 -0700

    Use .rst file extension for reStructuredText

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url='http://editorconfig.org/',
     license='LICENSE.txt',
     description='EditorConfig File Locator and Interpreter for Python',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     entry_points = {
         'console_scripts': [
             'editorconfig = editorconfig.main:main',
