fcaa82e2804f984d34b24de812af5ba6d8dcce7b
Trey Hunner <treyhunner@gmail.com>
2012-04-03 01:18:11 -0700

    Rename cli utility to editorconfig.py

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ setup(
     version=editorconfig.__version__,
     author='EditorConfig Team',
     packages=['editorconfig'],
+    namespace_packages=['editorconfig'],
     url='http://editorconfig.org/',
     license='LICENSE.txt',
     description='EditorConfig File Locator and Interpreter for Python',
     long_description=open('README.rst').read(),
     entry_points = {
         'console_scripts': [
-            'editorconfig = main:main',
+            'editorconfig.py = editorconfig:main',
         ]
     },
 )
