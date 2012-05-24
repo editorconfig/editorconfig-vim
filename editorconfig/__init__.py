6620bafcb246ea9eb614a0594dae9b3aaa4461bb
Trey Hunner <treyhunner@gmail.com>
2012-05-24 08:30:25 -0700

    Clarify docstrings and remove handler from exports

--- editorconfig/__init__.py
+++ editorconfig/__init__.py
@@ -1,20 +1,16 @@
-"""
-Modules exported by ``editorconfig`` package:
-
-- handler: used by plugins for locating and parsing EditorConfig files
-- exceptions: provides special exceptions used by other modules
-"""
+"""EditorConfig Python Core"""
 
 from versiontools import join_version
 
 VERSION = (0, 9, 0, "final")
 
-__all__ = ['get_properties', 'EditorConfigError', 'handler', 'exceptions']
+__all__ = ['get_properties', 'EditorConfigError', 'exceptions']
 
 __version__ = join_version(VERSION)
 
 
 def get_properties(filename):
+    """Locate and parse EditorConfig files for the given filename"""
     handler = EditorConfigHandler(filename)
     return handler.get_configurations()
 
