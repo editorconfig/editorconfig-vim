b4048b9a9ba9f7a9d1ac03e4a0d57c5c6e1b4471
Trey Hunner <treyhunner@gmail.com>
2012-06-28 09:24:50 -0700

    Fix version string format for development versions

--- editorconfig/versiontools.py
+++ editorconfig/versiontools.py
@@ -18,7 +18,7 @@ def join_version(version_tuple):
     """Return a string representation of version from given VERSION tuple"""
     version = "%s.%s.%s" % version_tuple[:3]
     if version_tuple[3] != "final":
-        version += ".%s" % version_tuple[3]
+        version += "-%s" % version_tuple[3]
     return version
 
 
