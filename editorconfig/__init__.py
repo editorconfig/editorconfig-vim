70a5fe8f9f7280a6a84410a5626dd99b70172f9c
Trey Hunner <trey@treyhunner.com>
2013-03-13 11:20:34 -0700

    Downgrade version to 0.11.1 since no core upgrade

--- editorconfig/__init__.py
+++ editorconfig/__init__.py
@@ -2,7 +2,7 @@
 
 from editorconfig.versiontools import join_version
 
-VERSION = (0, 12, 0, "final")
+VERSION = (0, 11, 1, "final")
 
 __all__ = ['get_properties', 'EditorConfigError', 'exceptions']
 
