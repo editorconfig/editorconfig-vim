01d2a1eea4695e9636a2cf07cb92976a7cbace5a
Hong Xu <hong@topbug.net>
2013-04-14 11:38:12 -0700

    Move java binding out.

--- java-binding/example/test-example.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SETLOCAL
-
-set LOCAL_CLASSPATH="../build/editorconfig.jar;.;%CLASSPATH%"
-javac -cp %LOCAL_CLASSPATH% TestEditorConfig.java
-java -cp %LOCAL_CLASSPATH% TestEditorConfig
-
-ENDLOCAL
