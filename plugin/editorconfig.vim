editorconfig/editorconfig-vim

commit 08bbf5875d987f102bd0ed6868dc4d86acf733a6

Add g:EditorConfig_max_line_indicator option.
This option gives user an option to choose how to indicator the maximal
line length.

plugin/editorconfig.vim
@@ -49,11 +49,16 @@ if !exists('g:EditorConfig_verbose')
     let g:EditorConfig_verbose = 0
 endif
 
+if !exists('g:EditorConfig_max_line_indicator')
+    let g:EditorConfig_max_line_indicator = 'line'
+endif
+
 if exists('g:EditorConfig_core_mode') && !empty(g:EditorConfig_core_mode)
     let s:editorconfig_core_mode = g:EditorConfig_core_mode
 else
     let s:editorconfig_core_mode = ''
 endif
+
 
 function! s:FindPythonInterp() " {{{1
 " Find python interp. If found, return python command; if not found, return ''
@@ -551,15 +556,20 @@ function! s:ApplyConfig(config) " {{{1
         endif
     endif
 
-    if has_key(a:config, 'max_line_length')
-        let l:max_line_length = str2nr(a:config['max_line_length'])
+    " highlight the columns following max_line_length
+    if exists('+colorcolumn')
+        if has_key(a:config, 'max_line_length')
+            let l:max_line_length = str2nr(a:config['max_line_length'])
 
-        if l:max_line_length > 0
-            let &l:textwidth = l:max_line_length
+            if l:max_line_length > 0
+                let &l:textwidth = l:max_line_length
 
-            " highlight the columns following max_line_length
-            if exists('+colorcolumn')
-                let &l:colorcolumn = join(range(l:max_line_length+1,&l:columns),',')
+                if g:EditorConfig_max_line_indicator == 'line'
+                    let &l:colorcolumn = l:max_line_length + 1
+                elseif g:EditorConfig_max_line_indicator == 'fill'
+                    let &l:colorcolumn = join(
+                                \ range(l:max_line_length+1,&l:columns),',')
+                endif
             endif
         endif
     endif

commit 06f9147c5d89a61b16397ab17cfd006fe850a0ac

Move '+colorcolumn' into the max_line_length handling block
08bbf5875d98 makes max_line_length ineffective if '+colorcolumn' is not
available. This commit fixes it.

Fixes: 08bbf5875d98

plugin/editorconfig.vim
@@ -557,13 +557,13 @@ function! s:ApplyConfig(config) " {{{1
     endif
 
     " highlight the columns following max_line_length
-    if exists('+colorcolumn')
-        if has_key(a:config, 'max_line_length')
-            let l:max_line_length = str2nr(a:config['max_line_length'])
+    if has_key(a:config, 'max_line_length')
+        let l:max_line_length = str2nr(a:config['max_line_length'])
 
-            if l:max_line_length > 0
-                let &l:textwidth = l:max_line_length
+        if l:max_line_length > 0
+            let &l:textwidth = l:max_line_length
 
+            if exists('+colorcolumn')
                 if g:EditorConfig_max_line_indicator == 'line'
                     let &l:colorcolumn = l:max_line_length + 1
                 elseif g:EditorConfig_max_line_indicator == 'fill'